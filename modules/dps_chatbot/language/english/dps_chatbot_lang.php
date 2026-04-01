2d5934458255f6bc72d94a51ba9ca015eb861411
ricardomagalhaes014 <54151298+ricardomagalhaes014@users.noreply.github.com>
2026-04-01 04:22:56 -0400

    fix urgente: criar ficheiro linguagem english dps_chatbot + remover LEFT JOIN customfieldsvalues

---
+++ modules/dps_chatbot/language/english/dps_chatbot_lang.php
@@ -0,0 +1,14 @@
+<?php
+$lang['dps_chatbot'] = 'Internal Chatbot';
+$lang['dps_chatbot_groups'] = 'Chat Groups';
+$lang['dps_chatbot_staff'] = 'Staff';
+$lang['dps_chatbot_new_group'] = 'Create New Group';
+$lang['dps_chatbot_group_name'] = 'Group Name';
+$lang['dps_chatbot_group_description'] = 'Description';
+$lang['dps_chatbot_is_public'] = 'Public Group';
+$lang['dps_chatbot_public'] = 'Public';
+$lang['dps_chatbot_select_chat'] = 'Select a contact or group to start chatting';
+$lang['dps_chatbot_type_message'] = 'Type your message...';
+$lang['dps_chatbot_send'] = 'Send';
+$lang['dps_chatbot_permission_manage_groups'] = 'Manage Chat Groups';
+$lang['dps_chatbot_group'] = 'Chat Group';
