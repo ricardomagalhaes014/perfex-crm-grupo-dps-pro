3e7abf8a563dc9a82e1a7214a7083fe1bbfff057
DPS Deploy <deploy@grupo-dps.com>
2026-04-13 08:03:01 -0400

    fix: adicionar language/english/promotores_lang.php obrigatório para activação

---
+++ modules/promotores/language/english/promotores_lang.php
@@ -0,0 +1,12 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+$lang['promotores']           = 'Promotores';
+$lang['promotor']             = 'Promotor';
+$lang['novo_promotor']        = 'New Promotor';
+$lang['editar_promotor']      = 'Edit Promotor';
+$lang['fase_contacto']        = 'Contact Phase';
+$lang['ultimo_contacto']      = 'Last Contact';
+$lang['distrito']             = 'District';
+$lang['importar_csv']         = 'Import CSV';
+$lang['carregar_base_inicial'] = 'Load Initial Database';
--- modules/promotores/language/portuguese/promotores_lang.php
+++ modules/promotores/language/portuguese/promotores_lang.php
@@ -2,4 +2,12 @@
 
 defined('BASEPATH') or exit('No direct script access allowed');
 
-$lang['promotores'] = 'Promotores';
+$lang['promotores']           = 'Promotores';
+$lang['promotor']             = 'Promotor';
+$lang['novo_promotor']        = 'Novo Promotor';
+$lang['editar_promotor']      = 'Editar Promotor';
+$lang['fase_contacto']        = 'Fase de Contacto';
+$lang['ultimo_contacto']      = 'Último Contacto';
+$lang['distrito']             = 'Distrito';
+$lang['importar_csv']         = 'Importar CSV';
+$lang['carregar_base_inicial'] = 'Carregar Base Inicial';
