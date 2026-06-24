321f4d3b271c821fd78864674d5c97924642866e
ricardomagalhaes014 <5.4151298e+07+ricardomagalhaes014@users.noreply.github.com>
2026-06-24 22:34:26 +0000

    fix: adicionar dps_voip_test.php ao deploy do dps_voip_db_setup

--- dps_voip_db_setup.php
+++ dps_voip_db_setup.php
@@ -43,6 +43,7 @@ if (isset($_GET['deploy'])) {
         'modules/dps_voip/models/Dps_voip_model.php',
         'modules/dps_voip/views/dps_voip/index.php',
         'modules/dps_voip/dps_voip.php',
+        'dps_voip_test.php',
     ];
     foreach ($files_to_deploy as $f) {
         $ch = curl_init($raw . $f);
